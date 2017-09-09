219a97acbd219327a16e583dd9ec1a25667cf923
Salim Oussayfi <box@slimou.net>
2017-09-09 17:48:36 +0200

    ...

---
+++ 004/IIFE/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//What is an IIFE and why might you use it?
+
+(function(){
+        var thing = {'hello':'main'};
+        console.log("main: ", thing);
+})();
---
+++ 004/IIFE/other.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//What is an IIFE and why might you use it?
+(function(){
+    var thing = {'hello': 'other'};
+    console.log("other: ", thing);
+})();
